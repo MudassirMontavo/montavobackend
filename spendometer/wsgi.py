6b6f56fe66da0418c01439cce17ce9d6239f9ff9
matt harley <matt@gramercystudios.com>
2013-08-30 11:29:54 +0800

    added dj static

--- spendometer/wsgi.py
+++ spendometer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for spendometer project.
+WSGI config for bird project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -18,15 +18,10 @@ import os
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "spendometer.settings"
+# os.environ["DJANGO_SETTINGS_MODULE"] = "bird.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spendometer.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+application = Cling(get_wsgi_application())
