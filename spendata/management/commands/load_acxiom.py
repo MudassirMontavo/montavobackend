3b12478bb9b2d04d167cb899315e3ba7d5669c26
matt harley <matt@gramercystudios.com>
2013-08-30 11:41:01 +0800

    added load acxiom data management command

---
+++ spendata/management/commands/load_acxiom.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from spendata.acxiom import read_into_db
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Loads Acxiom Data into the DB'
+
+    def handle(self, *args, **options):
+        try:
+            read_into_db()
+        except Exception, err:
+            logger.error("Error loading Acxiom Data into the DB: %s" % str(err))
+            raise CommandError("Error loading Acxiom Data into the DB: %s" % str(err))
+
+
